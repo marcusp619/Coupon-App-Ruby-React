1724457691de34917ff0eb1bd0f980103be9d346
marcusp619 <marcusp619@gmail.com>
2019-03-06 16:33:47 -0700

    Add components

---
+++ app/client/src/components/Details/Details.js
@@ -0,0 +1,7 @@
+import React, { Fragment } from 'react';
+
+function Details() {
+  return <Fragment>Details</Fragment>;
+}
+
+export default Details;
---
+++ app/client/src/components/Offer/Offer.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+function Offer() {
+  return (
+    <Fragment>
+      <h3>Offer</h3>
+    </Fragment>
+  );
+}
+
+export default Offer;
