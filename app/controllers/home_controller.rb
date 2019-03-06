8261d9fa51548112798198231cc12847df90a2a3
marcusp619 <marcusp619@gmail.com>
2019-03-06 13:37:07 -0700

    Add routes for data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
   def index
+    render json: Offer.all
   end
+
+  def show 
+    # require "pry"; binding.pry
+    render json: Offer.find(home_params[:id])
+  end
+
+  private
+    def home_params
+      params.permit(:id)
+    end
 end
