f6538e7302231b272818c38359a28c0dc4cdfc61
marcusp619 <marcusp619@gmail.com>
2019-03-06 21:44:51 -0700

    Add navbar background color

--- app/client/src/components/Navbar/Navbar.js
+++ app/client/src/components/Navbar/Navbar.js
@@ -5,28 +5,30 @@ import Nav from "react-bootstrap/Nav";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
+import styled from "styled-components";
 
 function Navbar() {
   return (
-    <NavBar bg="light" expand="lg">
+    <NavbarWrapper expand="lg">
       <NavBar.Brand>
         <Link to="/">ibotta</Link>
       </NavBar.Brand>
       <NavBar.Toggle aria-controls="basic-navbar-nav" />
       <NavBar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link>
-            <Link to="/">Offers</Link>
-          </Nav.Link>
-          <Nav.Link />
+          <Link to="/">Offers</Link>
         </Nav>
         <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
+          <StyledButton variant="outline-success">Search</StyledButton>
         </Form>
       </NavBar.Collapse>
-    </NavBar>
+    </NavbarWrapper>
   );
 }
 
+const StyledButton = styled(Button)``;
+const NavbarWrapper = styled(NavBar)`
+  background: rgb(75, 146, 141);
+`;
 export default Navbar;
